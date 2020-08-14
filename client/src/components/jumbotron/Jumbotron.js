b070e280bf6cd5facbde57a308551208cb7e8961
jdavis3333 <jdavis3333@gmail.com>
2020-08-14 13:59:49 -0700

    updated jumbotron text

--- client/src/components/jumbotron/Jumbotron.js
+++ client/src/components/jumbotron/Jumbotron.js
@@ -7,7 +7,8 @@ function Jumbotron({ children }) {
       className="jumbotron"
     >
       {children}
-      
+      <h1>(React) Google Books Search</h1>
+      <h4>Search for and Save Books of Interest</h4>
     </div>
   );
 }
