83ad8c8b295235072b816838c324fe2830bc0e87
Aidan Sweeny <apswee22@colby.edu>
2020-08-14 16:04:18 -0400

    Added delete btn

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import SavedResults from "../components/savedResults";
+import { SavedResults, SavedItem } from "../components/savedResults";
 import Nav from "../components/nav";
+import DeleteBtn from "../components/deleteBtn"
 import Jumbotron from "../components/jumbotron";
 import API from "../utils/API";
 
