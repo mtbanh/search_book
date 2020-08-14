41e60eccdb289cbc2f83feca99b327f37ed2c039
jdavis3333 <jdavis3333@gmail.com>
2020-08-14 11:38:41 -0700

    updated folder and file structure

---
+++ models/books.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+const bookSchema = new Schema({
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    description: String,
+    image: {type: String, default: ""},
+    link: {type: String, default: "", unique: true},
+  });
+  
+  const Book = mongoose.model("Book", bookSchema);
+  
+  module.exports = Book;
+  
