2f0d5c2b24f951e225badb9edd1adc505cddad91
Mai Banh <65269951+mtbanh@users.noreply.github.com>
2020-08-20 10:06:48 -0700

    merging

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,24 +1,13 @@
 const router = require("express").Router();
 const booksController = require("../../controllers/booksController");
-const axios = require ("axios");
 
-router.get("/books", (req,res)=>{
-  axios.get("https://www.googleapis.com/books/v1/volumes?q=" + query)
-  .then(({data: {result}}) =>res.json(results))
-  .catch(err =>console.log(err))
-});
+router.route("/")
+  .get(booksController.findAll)
+  .post(booksController.create);
 
-// Matches with "/api/books"
-// router.route("/search")
-//   .get(booksController.findAll)
-//   .post(booksController.create);
-
-// Matches with "/api/books/saved"
-// router
-//   .route("/saved")
-//   .get(booksController.findAll)
-// //   .put(booksController.update)
-//   .delete(booksController.remove);
-
+router.route("/:id")
+  .get(booksController.findById)
+  .put(booksController.update)
+  .delete(booksController.remove);
 
 module.exports = router;
