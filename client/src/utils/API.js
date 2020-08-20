3459e9b823b1b140685edcf2b68425723ccf964c
Mai Banh <65269951+mtbanh@users.noreply.github.com>
2020-08-20 10:40:09 -0700

    merging

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,27 +4,22 @@ import axios from "axios";
 // Export an object containing methods we'll use for accessing the Dog.Ceo API
 
 export default {
-  bookSearch: function(query) {
-    return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + query);
+  bookSearch: (bookTitle) => {
+    return axios.get(`https://www.googleapis.com/books/v1/volumes?q=title:${bookTitle}`);
   },
 
-  getBooks: ()=>{
+  getBook: ()=>{
     return axios.get("api/books");
   },
-  
-  // saveBook: () =>{
-  //   return axios.post("/api/books", savedBooks)
-  // },
-  getSaved: function() {
-    return axios.get("/api/books");
+  getBook : (id) =>{
+    return axios.get(`/api/books/${id}`)
   },
-  // Deletes the book with the given id
-  deleteSaved: function(id) {
-    return axios.delete("/api/books/" + id);
+  saveBook: (bookData) =>{
+    return axios.post("/api/books", bookData)
   },
-  // Saves a book to the database
-  createSave: function(bookData) {
-    return axios.post("/api/books", bookData);
 
+  // Deletes the book with the given id
+  deleteBook: function(id) {
+    return axios.delete(`/api/books/${id}`);
   }
 }
